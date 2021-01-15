ff90c9eb237442b5c47dd4f4bf47970832a0ed08
Santiago <santiagomusa@unisabana.edu.co>
2021-01-14 23:50:48 -0500

    basic template

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-body',
+    template: `<h1> Este es el bnody component </h1>`
+
+})
+export class BodyComponent {
+
+
+}
